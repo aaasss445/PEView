699cae1621eaa4225370d12a1e1227cfed7e6113
CabbageHotdog <aaasss445@naver.com>
2021-03-21 01:49:43 +0900

    IMAGE_SECTION_HEADER 추가, IMAGE_NT_HEADERS64 추가, 64비트 용으로 수정

--- CmdPEVeiw/PEStruct.h
+++ CmdPEVeiw/PEStruct.h
@@ -3,12 +3,14 @@
 #define VIEWBYTE 1
 #define VIEWANALYSIS 2
 #define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 16
+#define IMAGE_SIZEOF_SHORT_NAME 8
 
 typedef unsigned short WORD;
 typedef unsigned int LONG;
 typedef unsigned char CHAR;
 typedef unsigned long DWORD;
 typedef unsigned char BYTE;
+typedef unsigned long long ULONGLONG;
 
 
 typedef struct _IMAGE_DOS_HEADER { // 크기 64byte
@@ -86,3 +88,59 @@ typedef struct _IMAGE_NT_HEADERS {
 	IMAGE_FILE_HEADER FileHeader;
 	IMAGE_OPTIONAL_HEADER32 OptionalHeader;
 } IMAGE_NT_HEADERS32, *PIMAGE_NT_HEADERS32;
+
+typedef struct _IMAGE_SECTION_HEADER {
+	BYTE name[IMAGE_SIZEOF_SHORT_NAME];
+	union {
+		DWORD PhysicalAddress;
+		DWORD VirtualSize;
+	} Misc;
+	DWORD VirtualAddress;
+	DWORD SizeOfRawData;
+	DWORD PointerToRawData;
+	DWORD PointerToRelocations;
+	DWORD PointerToLinenumbers;
+	WORD NumberOfRelocations;
+	WORD NumberOfLinenumbers;
+	DWORD Characteristics;
+} IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
+
+typedef struct _IMAGE_OPTIONAL_HEADER64 {
+	WORD        Magic;
+	BYTE        MajorLinkerVersion;
+	BYTE        MinorLinkerVersion;
+	DWORD       SizeOfCode;
+	DWORD       SizeOfInitializedData;
+	DWORD       SizeOfUninitializedData;
+	DWORD       AddressOfEntryPoint;
+	DWORD       BaseOfCode;
+	ULONGLONG   ImageBase;
+	DWORD       SectionAlignment;
+	DWORD       FileAlignment;
+	WORD        MajorOperatingSystemVersion;
+	WORD        MinorOperatingSystemVersion;
+	WORD        MajorImageVersion;
+	WORD        MinorImageVersion;
+	WORD        MajorSubsystemVersion;
+	WORD        MinorSubsystemVersion;
+	DWORD       Win32VersionValue;
+	DWORD       SizeOfImage;
+	DWORD       SizeOfHeaders;
+	DWORD       CheckSum;
+	WORD        Subsystem;
+	WORD        DllCharacteristics;
+	ULONGLONG   SizeOfStackReserve;
+	ULONGLONG   SizeOfStackCommit;
+	ULONGLONG   SizeOfHeapReserve;
+	ULONGLONG   SizeOfHeapCommit;
+	DWORD       LoaderFlags;
+	DWORD       NumberOfRvaAndSizes;
+	// DataDirectory[1].VirtualAddress가 IMAGE_IMPORT_DESCRIPTOR 구조체 배열이 있는 곳이다.
+	IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER64, * PIMAGE_OPTIONAL_HEADER64;
+
+typedef struct _IMAGE_NT_HEADERS64 {
+	DWORD                   Signature;
+	IMAGE_FILE_HEADER       FileHeader;
+	IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+} IMAGE_NT_HEADERS64, * PIMAGE_NT_HEADERS64;
--- CmdPEVeiw/PEView.c
+++ CmdPEVeiw/PEView.c
@@ -3,16 +3,17 @@
 #include"PEStruct.h"
 
 IMAGE_DOS_HEADER idh;
-IMAGE_NT_HEADERS32 inh;
+IMAGE_NT_HEADERS64 inh;
 
 // notepad.exe 경로
 // C:\\Windows\\notepad.exe
 
 void PrintPEView(FILE* fp, int FileSize, int Option); // 옵션에 따라 화면에 다르게 출력해주는 함수
 void IMAGE_DOS_HEADER_PRINT(FILE* fp); // 분석한걸 옵션으로 받았을때 IMAGE_DOS_HEADER 구조체를 출력해주는 함수
-void IMAGE_NT_HEADERS32_PRINT(FILE* fp);
+void IMAGE_NT_HEADERS64_PRINT(FILE* fp);
 void DOS_HEADER_PRINT(FILE* fp);
 void ChangeEscapeSequence(CHAR* ch);
+void IMAGE_SECTION_HEADER_PRINT(FILE* fp);
 //void PrintText(int* p);
 
 int main(int argc, int *argv[]) {
@@ -76,27 +77,25 @@ void PrintPEView(FILE* fp, int FileSize, int Option) {
 				}
 				printf("\n");
 			}
-			if (i > 1006) { // 그냥 임시로 끊어 준것
-				break;
-			}
+			//if (i > 1006) { // 그냥 임시로 끊어 준것
+			//	break;
+			//}
 		}
 		free(buffer); // 메모리 해제
 	}
 	// 분석한 걸 출력
 	if (Option == VIEWANALYSIS) {
-		//printf("%X\n", ftell(fp));
+		printf("%d\n", sizeof(inh));
 		IMAGE_DOS_HEADER_PRINT(fp);
-		//printf("%X\n", ftell(fp));
 		DOS_HEADER_PRINT(fp);
-		//printf("%X\n", ftell(fp));
-		IMAGE_NT_HEADERS32_PRINT(fp);
-		//printf("%X\n", ftell(fp));
+		IMAGE_NT_HEADERS64_PRINT(fp);
+		IMAGE_SECTION_HEADER_PRINT(fp);
 	}
 
 }
 
 void IMAGE_DOS_HEADER_PRINT(FILE* fp) {
-	fread(&idh, sizeof(idh), 1, fp);
+	fread(&idh, sizeof(idh), 1, fp); // 파일 읽기
 	printf("+++IMAGE_DOS_HEADER+++\n");
 	printf("e_magic : %04X\n", idh.e_magic);
 	printf("e_cblp : %04X\n", idh.e_cblp);
@@ -117,15 +116,13 @@ void IMAGE_DOS_HEADER_PRINT(FILE* fp) {
 	}
 	printf("e_oeminfo : %04X\n", idh.e_oeminfo);
 	for (int i = 0; i < 10; i++) {
-		printf("e_res2[%d] : %04X\n", i, idh.e_res2[i]);
+		printf("e_res2[%X] : %04X\n", i, idh.e_res2[i]);
 	}
 	printf("e_lfanew : %08X\n", idh.e_lfanew);
 }
 
-void IMAGE_NT_HEADERS32_PRINT(FILE* fp) {
-	//printf("%d\n", sizeof(inh));
-	fseek(fp, idh.e_lfanew, SEEK_SET);
-	//printf("%X\n", ftell(fp));
+void IMAGE_NT_HEADERS64_PRINT(FILE* fp) {
+	fseek(fp, idh.e_lfanew, SEEK_SET); // 파일 포인터가 자꾸 이상해져서 아예 초기화 했음
 	fread(&inh, sizeof(inh), 1, fp);
 	printf("+++IMAGE_NT_HEADERS+++\n");
 	printf("Signature : %08X\n", inh.Signature);
@@ -146,8 +143,7 @@ void IMAGE_NT_HEADERS32_PRINT(FILE* fp) {
 	printf("SizeOfUninitializedData : %08X\n", inh.OptionalHeader.SizeOfUninitializedData);
 	printf("AddressOfEntryPoint : %08X\n", inh.OptionalHeader.AddressOfEntryPoint);
 	printf("BaseOfCode : %08X\n", inh.OptionalHeader.BaseOfCode);
-	printf("BaseOfData : %08X\n", inh.OptionalHeader.BaseOfData);
-	printf("ImageBase : %08X\n", inh.OptionalHeader.ImageBase);
+	printf("ImageBase : %016X\n", inh.OptionalHeader.ImageBase);
 	printf("SectionAlignment : %08X\n", inh.OptionalHeader.SectionAlignment);
 	printf("FileAlignment : %08X\n", inh.OptionalHeader.FileAlignment);
 	printf("MajorOperatingSystemVersion : %04X\n", inh.OptionalHeader.MajorOperatingSystemVersion);
@@ -162,28 +158,53 @@ void IMAGE_NT_HEADERS32_PRINT(FILE* fp) {
 	printf("CheckSum : %08X\n", inh.OptionalHeader.CheckSum);
 	printf("Subsystem : %04X\n", inh.OptionalHeader.Subsystem);
 	printf("DllCharacteristics : %04X\n", inh.OptionalHeader.DllCharacteristics);
-	printf("SizeOfStackReserve : %08X\n", inh.OptionalHeader.SizeOfStackReserve);
-	printf("SizeOfStackCommit : %08X\n", inh.OptionalHeader.SizeOfStackCommit);
-	printf("SizeOfHeapReserve : %08X\n", inh.OptionalHeader.SizeOfHeapReserve);
-	printf("SizeOfHeapCommit : %08X\n", inh.OptionalHeader.SizeOfHeapCommit);
+	printf("SizeOfStackReserve : %016X\n", inh.OptionalHeader.SizeOfStackReserve);
+	printf("SizeOfStackCommit : %016X\n", inh.OptionalHeader.SizeOfStackCommit);
+	printf("SizeOfHeapReserve : %016X\n", inh.OptionalHeader.SizeOfHeapReserve);
+	printf("SizeOfHeapCommit : %016X\n", inh.OptionalHeader.SizeOfHeapCommit);
 	printf("LoaderFlags : %08X\n", inh.OptionalHeader.LoaderFlags);
 	printf("NumberOfRvaAndSizes : %08X\n", inh.OptionalHeader.NumberOfRvaAndSizes);
 	for (int i = 0; i < IMAGE_NUMBEROF_DIRECTORY_ENTRIES; i++) {
-		printf("DataDirectory[%d] - VirtualAddress : %08X\n", i, inh.OptionalHeader.DataDirectory[i].VirtualAddress);
-		printf("DataDirectory[%d] - Size : %08X\n", i, inh.OptionalHeader.DataDirectory[i].Size);
+		printf("DataDirectory[%X] - VirtualAddress : %08X\n", i, inh.OptionalHeader.DataDirectory[i].VirtualAddress);
+		printf("DataDirectory[%X] - Size : %08X\n", i, inh.OptionalHeader.DataDirectory[i].Size);
 	}
 }
 
+void IMAGE_SECTION_HEADER_PRINT(FILE* fp) {
+	int IMAGE_SECTION_HEADER_START = idh.e_lfanew + sizeof(inh); // IMAGE_SECTION_HEADER의 시작 부분 저장
+	int IMAGE_SECTION_HEADER_COUNT = inh.FileHeader.NumberOfSections; // IMAGE_SECTION_HEADER의 개수 저장
+	IMAGE_SECTION_HEADER* ish = (IMAGE_SECTION_HEADER*)malloc(sizeof(IMAGE_SECTION_HEADER) * (IMAGE_SECTION_HEADER_COUNT)); // 개수만큼 배열 생성
+
+	fseek(fp, IMAGE_SECTION_HEADER_START, SEEK_SET);
+	for (int i = 0; i < IMAGE_SECTION_HEADER_COUNT; i++) {
+		fread(&ish[i], sizeof(IMAGE_SECTION_HEADER), 1, fp);
+		printf("+++IMAGE_SECTION_HEADER[%d]+++\n", i);
+		printf("Name : %s\n", ish[i].name);
+		printf("VirtualSize : %08X\n", ish[i].Misc.VirtualSize);
+		printf("VirtualAddress : %08X\n", ish[i].VirtualAddress);
+		printf("SizeofRawData : %08x\n", ish[i].SizeOfRawData);
+		printf("PointerToRawData : %08X\n", ish[i].PointerToRawData);
+		printf("PointerToRelocations : %08X\n", ish[i].PointerToRelocations);
+		printf("PointerToLinenumber : %08X\n", ish[i].PointerToLinenumbers);
+		printf("NumberOfRelocations : %04X\n", ish[i].NumberOfRelocations);
+		printf("NumberOfLinenumbers : %04X\n", ish[i].NumberOfLinenumbers);
+		printf("Characteristics : %08X\n", ish[i].Characteristics);
+	}
+	
+	free(ish);
+}
+
 void DOS_HEADER_PRINT(FILE* fp) {
-	int DOS_HEADER_SIZE = idh.e_lfanew - sizeof(IMAGE_DOS_HEADER);
-	char* DosHeaderBuffer = (char*)malloc(sizeof(char) * (DOS_HEADER_SIZE));
+	fseek(fp, sizeof(idh), SEEK_SET); // 파일 위치 설정
+	int DOS_HEADER_SIZE = idh.e_lfanew - sizeof(IMAGE_DOS_HEADER); // DOS_HEADER의 크기 저장
+	char* DosHeaderBuffer = (char*)malloc(sizeof(char) * (DOS_HEADER_SIZE)); // DOS_HEADER의 값을 받을 공간 만듦
 	fread(DosHeaderBuffer, DOS_HEADER_SIZE, 1, fp);
 	printf("+++DOS_HEADER+++\n");
 	for (int i = 0; i < DOS_HEADER_SIZE; i++) {
 		if (i % 16 == 0) {
-			printf("%08x\t", sizeof(IMAGE_DOS_HEADER) + i);
+			printf("%08x\t", sizeof(IMAGE_DOS_HEADER) + i); // 파일 커서 위치 출력
 		}
-		ChangeEscapeSequence(&DosHeaderBuffer[i]);
+		ChangeEscapeSequence(&DosHeaderBuffer[i]); // 몇몇 특수문자 같은거 제거
 
 		if (i % 16 == 15) {
 			printf("\n");
@@ -193,10 +214,10 @@ void DOS_HEADER_PRINT(FILE* fp) {
 	free(DosHeaderBuffer);
 }
 
-void ChangeEscapeSequence(CHAR *ch) {
-	if (*ch == 0x0A || *ch == 0x0D ||
-		*ch == 0x0 || *ch == 0x09 ||
-		ch == 0x0B || *ch== 0x07 ||
+void ChangeEscapeSequence(CHAR *ch) { // 몇몇 글자들 대체하기
+	if (*ch == 0x0A || *ch == 0x0D || // 개행, 공백, 백스페이스, 수평 탭, 수직 탭, 처음으로 가기, 경보, 등등
+		*ch == 0x0 || *ch == 0x09 ||  // 그냥 .으로 바꿈
+		*ch == 0x0B || *ch== 0x07 ||
 		*ch == 0x08) {
 		printf(".");
 	}
